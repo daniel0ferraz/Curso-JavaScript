ec6046754fad0ce72005db7345c7da2a909a7c4f
Daniel Ferraz <daniel0ferraz@outlook.com>
2020-06-27 16:33:32 -0300

    adicionando arquivos e iniciando o capitulo Funções

---
+++ Controle/if.js
@@ -0,0 +1,10 @@
+let nota1 = 0
+let nota2 = 0
+
+function Media(nota1, nota2) {
+    
+}
+
+
+Media(7, 5)
+let media = nota1 + nota2 / 2
---
+++ Função/cidadaoPrimeiraClasse.js
@@ -0,0 +1,32 @@
+// Função em JS é First-Class Object (Citizens)
+// Higher-order function
+
+// Criar de forma literal 
+function fun1() { }
+
+// Armazenar em uma variável
+const fun2 = function ()  {}
+
+// Armazenar em um array 
+const array = [function (a, b) { return a + b}, fun1, fun2]
+console.log(array[0](2, 3))
+
+// Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function () {return 'opa' }
+console.log(obj.falar())
+
+// Passar função como parametro
+function run(fun) {
+   fun()
+}
+run(function () { console.log('Executando...') })
+
+// Uma função pode retornar/conter um funcao
+
+function some(a, b) {
+    return function(c) {
+        console.log(a + b + c )
+    }
+}
+SVGZoomAndPan(2, 3)(4)
