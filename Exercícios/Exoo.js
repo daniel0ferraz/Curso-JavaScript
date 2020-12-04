8ccc808f9717ed1c01436801bc02b7aa49e3106f
Daniel Ferraz <daniel0ferraz@outlook.com>
2020-12-04 14:14:18 -0300

    praticando sintaxe OO

---
+++ Exercícios/Exoo.js
@@ -0,0 +1,52 @@
+const pessoa = {
+  nome: 'Daniel Ferraz',
+  idade: 12,
+  altura: 1.7,
+
+  endereco: {
+    logradouro: 'Rua marques de barbaceba',
+    numero: 81,
+    bairro: 'Centro',
+    complemento: 'casa',
+    estado: 'Bahia',
+  },
+  moradores: [
+    {
+      nome: 'Sidelvan',
+      idade: 60,
+      grau: 'Pai',
+    },
+    {
+      nome: 'ketilla',
+      idade: 30,
+      grau: 'irmã',
+    },
+    {
+      nome: 'taynan',
+      idade: 33,
+      grau: 'irmã',
+    },
+    {
+      nome: 'Lúcia',
+      idade: 60,
+      grau: 'Mãe',
+    },
+    {
+      nome: 'Isac',
+      idade: '20',
+      grau: 'irmão',
+    },
+  ],
+};
+console.log(pessoa.moradores.length);
+console.log(pessoa.moradores);
+console.log(pessoa);
+
+Produto = {
+  nome: 'Toner',
+  valor: 20.00,
+  quantidade: 6
+}
+
+console.log(Produto.valor * Produto.quantidade)
+
