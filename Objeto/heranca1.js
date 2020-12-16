81f522263960431c861396aec4a2cfc4aa6f2a11
Daniel Ferraz <Daniel0ferraz@outlook.com>
2020-12-16 00:21:45 -0300

    oo

---
+++ Objeto/heranca1.js
@@ -0,0 +1,12 @@
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324 
+}
+
+const volvo = {
+    modelo: 'V40',
+    velMax: 200
+}
+
+console.log(ferrari.__proto__)
+console.log(ferrari.__proto__ === Object.prototype)
