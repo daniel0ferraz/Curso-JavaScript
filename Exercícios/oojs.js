f5b543fa58d3bd513b3498c88868d87079dcd658
Daniel Ferraz <Daniel0ferraz@outlook.com>
2020-12-22 09:35:12 -0300

    Praticando Objetos em js 03

---
+++ Exercícios/oojs.js
@@ -0,0 +1,22 @@
+var pessoa = {
+    nome: 'Daniel Ferraz',
+    idade: 21,
+    sexo: 'Masculino',
+    interesses: ['musicas, ciclismo, aventuras'],
+
+    bio() {
+        console.log(this.nome +' Tem '+ this.idade+ ", é do sexo "+this.sexo+" possui os seguintes interesses"+this.interesses)
+    }, suadacao() {
+        console.log('Olá, sou o '+this.nome + '!')
+    }
+};
+
+pessoa.bio();
+pessoa.suadacao();
+pessoa.idade
+console.log(pessoa)
+
+//podemos também setar (atualizar) o valor de membros de objetos simplesmente declarando o membro que queremos setar (usando notação de ponto ou colchete), tipo assim:
+pessoa['olhos'] = 'castanhos escuros';
+pessoa.idade = 18;
+console.log(pessoa)
