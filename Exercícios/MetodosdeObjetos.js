b511bb79cb5f0ddd006dfb29ce75ac8a8cae619e
Daniel Ferraz <Daniel0ferraz@outlook.com>
2020-12-29 23:10:04 -0300

    update

--- Exercícios/MetodosdeObjetos.js
+++ Exercícios/MetodosdeObjetos.js
@@ -1,7 +1,10 @@
 // manipulando objetos 
 const  destino = { a: 1, b: 2};
 const origem = {b: 4, c: 5};
-
 const denstinoRetornado = Object.assign(destino, origem)
 
 console.log(denstinoRetornado, destino)
+
+// clonagem de objetos
+const obj = {a:1};
+const copia = Object.assign({}, obj)
