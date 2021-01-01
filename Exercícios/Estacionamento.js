7ea6530a4c9035c5de184e8c212a48fcf9dd096a
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-01-01 18:35:19 -0300

    add new  xercicio

---
+++ Exercícios/Estacionamento.js
@@ -0,0 +1,18 @@
+var veiculo = [{
+    modelo: 'Toyota Hilux srv',
+    placa: 'qwq-3025',
+    cor: 'Preta'
+},
+{
+    modelo: 'Hiunday Elantra',
+    placa: 'qjv-3025',
+    cor: 'Vermelho' 
+},{
+    modelo: 'S-10 highcountry',
+    placa: 'jqj-2865',
+    cor: 'Prata'
+}];
+
+console.log(veiculo)
+veiculo.modelo
+
