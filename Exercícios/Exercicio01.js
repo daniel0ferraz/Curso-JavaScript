7d78d4ca18daab38e04a9d485dd374cfb3c257ef
Daniel Ferraz <Daniel0ferraz@outlook.com>
2020-12-31 23:23:40 -0300

    update files folder exercicios

--- Exercícios/Exercicio01.js
+++ Exercícios/Exercicio01.js
@@ -3,3 +3,22 @@ function calculadora(op1, op2) {
 }
 
 calculadora(4, 5)
+
+function dobro(n1) {
+   return n1 * 2;
+}
+
+console.log(dobro(3))
+
+ Media = (nota1, nota2) => {
+   return (nota1 + nota2 ) / 2;
+   if (Media() > 7) {
+       console.log("Aprovado")
+   }
+   else {
+    console.log("Reprovado")
+   }
+    
+ }
+
+Media(5, 6)
