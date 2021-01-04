b7d0225266d0d4e81d4ffa645c7afbf2cd6c0b69
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-01-04 10:11:54 -0300

    Herança exemplo3

---
+++ Objeto/objeto3.js
@@ -0,0 +1,24 @@
+// classe
+const pai ={ nome: 'Felipe', corCabelo: 'Preto'}
+
+//objeto 
+const filha1 = Object.create(pai)
+filha1.nome = 'Ana'
+console.log(filha1.corCabelo)
+
+const filha2 = Object.create(pai, {
+    nome: {value: 'Bia', writable: false, enumerable: true}
+})
+
+console.log(filha2.nome)
+filha2.nome = 'Carla'
+console.log(`${filha2.nome} tem cabelo ${filha2.corCabelo}`)
+
+console.log(Object.keys(filha1))
+console.log(Object.keys(filha2))
+
+for(let key in filha2) {
+    //console.log(key)
+    filha2.hasOwnProperty(key)
+    console.log(key) : console.log(`Por herança ${key}`)
+}
