006b53d76244687923ceb584e012eec34901a07a
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-01-11 18:38:07 -0300

    praticando

--- Objeto/heranca6.js
+++ Objeto/heranca6.js
@@ -9,7 +9,7 @@ const aula1 = new Aula('Bem Vindo', 123)
 const aula2 = new Aula('Até breve', 456)
 console.log(aula1, aula2)
 
-// simular o operador new
+// Simulando o operador New
 function novo(f, ...params) {
     const obj = {}
     obj.__proto__ = f.prototype
