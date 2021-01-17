5a85e5c50a26e9c2e3a597f9f8cb46233fcdbbac
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-01-16 22:20:34 -0300

    Praticando exercicios Caixa02

---
+++ Exercícios/Caixa02.js
@@ -0,0 +1,29 @@
+const Contabancaria = {
+    titular: 'Daniel Ferraz',
+    conta: 0100,
+    agencia: 'Nupagamentos',
+    saldo: 1000.00
+
+    function transacoes(opcao, valor) {
+
+        switch(opcao) {
+            case 1:
+               saldo += valor
+               console.log(`Depósito: $+${valor}`)
+               console.log(`Saldo Atualizado: ${saldo}`)
+            break
+        
+            case 2:
+                saldo -= valor
+                console.log(`Saque: -$${valor}`)
+                console.log(`Saldo Atualizado: ${saldo}`)
+            break
+        
+            default:
+              console.log("Valor Invlido")
+    
+        }
+    }
+}
+
+console.log(Contabancaria)
