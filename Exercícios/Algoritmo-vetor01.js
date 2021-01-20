0d243c62fa7b96aeb321fe856c61401ec2897b2e
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-01-20 16:48:23 -0300

    Praticando exercicios algoritmos vetores

---
+++ Exercícios/Algoritmo-vetor01.js
@@ -0,0 +1,13 @@
+var s = ['A', 'B', 'A', 'C', 'A', 'T', 'E']
+var i =0
+var k=0
+
+for ( i = 0; i < s; i++) {
+   if (s[i] = 'A') {
+       k = k + 1;
+   }
+    
+}
+
+console.log(`O caractere A ocorre: ${k} vezes no string`)
+
---
+++ Exercícios/Algoritmo_vetor06.js
@@ -0,0 +1,12 @@
+var X
+var i 
+var j
+
+X = 0;
+for (i = 0; i < 5; i++) {
+  for (j = 1; j < 2; j++) {
+        X = X + j+i;
+  }  
+}
+
+console.log(X)
