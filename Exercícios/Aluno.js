926d355d6572a34b665eba2fdc95df5d7c03e5fc
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-04-04 23:05:23 -0300

    Atualizando files e praticando oo

--- Exercícios/Aluno.js
+++ Exercícios/Aluno.js
@@ -3,18 +3,19 @@ function Aluno(nome, matricula, n1, n2, n3, media) {
         nome,
         matricula,
         n1, n2, n3,
-    
+
         getMedia() {
             console.log("---Resultado---")
             console.log(`Av1: ${n1} Av2: ${n2}`)
             console.log("---Media---")
-            return  media =( n1 + n2 + n3 ) / 3;
-    
+            return media = (n1 + n2 + n3) / 3;
+
         }
     }
 }
 
 const a1 = new Aluno("Daniel", 2020100646, 4, 9, 10)
-const a2 = new Aluno("Felipe",2020100647, 8, 7, 8 )
+const a2 = new Aluno("Felipe", 2020100647, 8, 7, 8)
 const a3 = new Aluno("Daniela Nascimento Prates", 2020100648, 10, 9.0, 9.5)
 console.log(a1, a2, a3)
+console.log(a1.getMedia())
--- Exercícios/Alunos.js
+++ Exercícios/Alunos.js
@@ -1,26 +1,28 @@
 class Alunos {
-    constructor(nome, sexo, curso, endereco, telefone) {
+    constructor(nome, sexo, curso, endereco, telefone, matricula) {
         this.nome = nome;
         this.sexo = sexo;
         this.curso = curso;
         this.endereco = endereco;
         this.telefone = telefone;
+        this.matricula = matricula;
     }
 
     setNome = () => {
-
+        this.nome = nome;
     }
     setMaticula = () => {
-
+        this.matricula = matricula;
     }
     getNome = () => {
         return this.nome;
     }
     getMatricula = () => {
-        return this.nome;
+        return this.matricula;
     }
 }
 
 
-const aluno1 = new Alunos("Maria Tereza", "Feminino", "Pedagogia", "ABC", 7399542211);
+const aluno1 = new Alunos("Tereza", "Feminino", "Pedagogia", "ABC", 7399542211, 2020100646);
 console.log(aluno1)
+console.log(`O Aluno(a): ${aluno1.getNome()}, possui a matricula:${aluno1.getMatricula()}`)
