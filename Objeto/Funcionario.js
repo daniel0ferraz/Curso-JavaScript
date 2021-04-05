6664286990f00153b157f9ed06b77f9d10eee2c1
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-04-05 16:53:33 -0300

    praticando polimorfismo sobreescrita e sobrecarga

---
+++ Objeto/Funcionario.js
@@ -0,0 +1,57 @@
+class Funcionario {
+  constructor(nome, valorHora, horasTrabalhadas) {
+    this.nome = nome;
+    this.valorHora = valorHora;
+    this.horasTrabalhadas = horasTrabalhadas;
+  }
+
+  setNome() {
+    this.nome = nome;
+  }
+
+  setValorHora() {
+    this.valorHora = valorHora;
+  }
+
+  setHorasTrab() {
+    this.horasTrabalhadas = horasTrabalhadas;
+  }
+
+  calculaSalario = () => {
+    return this.valorHora * this.horasTrabalhadas;
+  }
+}
+
+
+class Gerente extends Funcionario {
+  constructor(bonus) {
+    this.bonus = bonus;
+  }
+
+  setBonus() {
+    this.bonus = bonus;
+  }
+
+  calculaSalario() {
+    return this.valorHora * this.horasTrabalhadas + this.bonus
+  }
+}
+
+class Pagamento {
+  constructor() {
+    const g1 = new Gerente()
+    g1.setNome("Daniel Ferraz")
+    g1.setHorasTrab(250)
+    g1.valorHora(150)
+    g1.setBonus(1000)
+    console.log(g1.getNome(), g1.calculaSalario())
+
+    const f1 = new Funcionario()
+    f1.setNome("Maria Tereza")
+    f1.setHorasTrab(250)
+    f1.setValorHora(80)
+    console.log(`${f1.getNome()} tem o salario R$ ${f1.calculaSalario()}`)
+  }
+}
+
+
