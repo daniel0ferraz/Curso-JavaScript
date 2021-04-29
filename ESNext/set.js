d94226312861f27f1139bf3c3c1810bf14bbe4b5
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-04-28 23:05:13 -0300

    Praticando ES

---
+++ ESNext/set.js
@@ -0,0 +1,18 @@
+// não aceita repetição/ não indexada
+
+const times = new Set();
+times.add('Vasco')
+times.add('São Paulo').add('Palmeiras').add('Corinthians')
+times.add('Flamengo')
+times.add('Vasco')
+
+console.log(times)
+console.log(times.size)
+console.log(times.has('vasco'))
+console.log(times.has('Vasco'))
+times.delete('Flamengo')
+console.log(times.has('Flamengo'))
+
+const nomes = ['Daniel', 'Lucas', 'Julia', 'Lucas', 'Daniel']
+const nomesSet = new Set(nomes)
+console.log(nomesSet)
